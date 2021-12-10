90a8ab6688a0d4f236f891c38048bb48e01de65c
alessandroboeckle <81869814+alessandroboeckle@users.noreply.github.com>
2021-12-10 22:15:16 +0100

    Neues Datum bekommen
    Aktuelles Datum bekommen

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const calendarButton = document.querySelector(".btn-start");
 const calendarContainer = document.querySelector(".container");
 
 const calendarDays = 24;
+var date1 = new Date();
+date1 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate(), 0, 0, 0);
+console.log(date1);
 
 const openDoor = (path, event) => {
     event.target.parentNode.style.backgroundImage = `url(${path})`;
@@ -13,6 +16,8 @@ const createCalendar = () => {
     for(let i = 0; i  < calendarDays; i++) {
         const calendarDoor = document.createElement("div");
         const calendarDoorText = document.createElement("div");
+
+
 
         calendarDoor.classList.add("image");
         calendarDoor.style.gridArea = "door" + (i + 1);
